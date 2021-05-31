adadc90ba723fe7a0af34fab243b7f2fcb665bd8
sekim <sekim@dtwocorp.com>
2021-05-31 13:51:05 +0900

    create about css and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React from "react";  
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from './routes/home';
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
-  return <HashRouter>
-    <Route path="/" exact={true} component={Home} />
-    <Route path="/About" component={About} />
-  </HashRouter>;
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/About" component={About} />
+    </HashRouter>
+  
+    );
+
 }
 
 export default App;
